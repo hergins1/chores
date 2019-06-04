13ef9ce950cce5302017f8890613687ac743e57a
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-06-04 13:06:11 -0500

    users create/read/update/delete finished

--- routes/usersApiRoutes.js
+++ routes/usersApiRoutes.js
@@ -1,53 +1,10 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // GET all Households
-  app.get("/api/households", function(req, res) {
-    db.Households.findAll({}).then(function(dbHouseholds) {
-      res.json(dbHouseholds);
-    });
-  });
-
   // GET all Users
   app.get("/api/users", function(req, res) {
     db.Users.findAll({}).then(function(dbUsers) {
       res.json(dbUsers);
-    });
-  });
-
-  // GET all Chores
-  app.get("/api/chores", function(req, res) {
-    db.Chores.findAll({}).then(function(dbChores) {
-      res.json(dbChores);
-    });
-  });
-
-  // GET specific Chore
-  app.get("/api/chores/:id", function(req, res) {
-    db.Chores.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbChores) {
-      res.json(dbChores);
-    });
-  });
-
-  // GET completed Chores
-  app.get("/api/chores/completed", function(req, res) {
-    db.Chores.findAll({
-      where: {
-        completed: true
-      }
-    }).then(function(dbChores) {
-      res.json(dbChores);
-    });
-  });
-
-  // CREATE new Households
-  app.post("/api/households", function(req, res) {
-    db.Households.create(req.body).then(function(dbHouseholds) {
-      res.json(dbHouseholds);
     });
   });
 
@@ -58,50 +15,37 @@ module.exports = function(app) {
     });
   });
 
-  // CREATE new Chores
-  app.post("/api/chores", function(req, res) {
-    db.Chores.create(req.body).then(function(dbChores) {
-      res.json(dbChores);
-    });
-  });
-
-  // UPDATE Users
-
-  // UPDATE Chores
-  app.put("/api/chores", function(req, res) {
-    db.Chores.update(req.body.completed, {
-      where: {
-        id: req.body.id
+  // UPDATE Users name
+  app.put("/api/users/:id", function(req, res) {
+    db.Users.update(
+      {
+        name: req.body.name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
       }
-    }).then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
-
-  // DELETE Households
-  app.delete("/api/households/:id", function(req, res) {
-    db.Households.destroy({ where: { id: req.params.id } }).then(function(
-      dbHouseholds
-    ) {
-      res.json(dbHouseholds);
-    });
-  });
-
-  // DELETE Users
-  app.delete("/api/users/:id", function(req, res) {
-    db.Users.destroy({ where: { id: req.params.id } }).then(function(
-      dbUsers
-    ) {
+    ).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
 
-  // DELETE Chores
-  app.delete("/api/chores/:id", function(req, res) {
-    db.Chores.destroy({ where: { id: req.params.id } }).then(function(
-      dbChores
-    ) {
-      res.json(dbChores);
+  // router.put(‘/book/:bookId’, function (req, res, next) {
+  //   Book.update(
+  //     {title: req.body.title},
+  //     {where: req.params.bookId}
+  //   )
+  //   .then(function(rowsUpdated) {
+  //     res.json(rowsUpdated)
+  //   })
+  //   .catch(next)
+  //  })
+
+  // DELETE Users
+  app.delete("/api/users/:id", function(req, res) {
+    db.Users.destroy({ where: { id: req.params.id } }).then(function(dbUsers) {
+      res.json(dbUsers);
     });
   });
 };
