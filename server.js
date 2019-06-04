a7f0f11a4ee583811a2aa98471fba93e24d14154
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-06-04 15:43:11 -0500

    photos model and api routes created

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
+require("./routes/choresApiRoutes")(app);
+require("./routes/householdsApiRoutes")(app);
+require("./routes/usersApiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: true };
