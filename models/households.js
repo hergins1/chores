3d03ca7e40e793559cff8031933a365325554704
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-06-08 13:05:00 -0500

    updated Households table with email and password

--- models/households.js
+++ models/households.js
@@ -6,6 +6,20 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: [1]
       }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     }
   });
 
--- models/users.js
+++ models/users.js
@@ -29,7 +29,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [6]
+        len: [1]
       }
     },
     photo: {
