8f08328c9568f59e809dd63508c27957cef12722
Joshua Bradley <bradleyjd19@gmail.com>
2019-06-08 11:54:19 -0500

    added new chore and new user pages/updated database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,24 @@
 Use chores_app;
 
-INSERT INTO Households (name, createdAt, updatedAt) VALUES ("Anderson", CURDATE(), CURDATE());
-INSERT INTO Households (name, createdAt, updatedAt) VALUES ("Smith", CURDATE(), CURDATE());
+INSERT INTO Households (name, createdAt, updatedAt) 
+VALUES 
+("Anderson", CURDATE(), CURDATE()),
+("Smith", CURDATE(), CURDATE());
 
-INSERT INTO Users (name, age, admin, createdAt, updatedAt, HouseholdId) VALUES ("Rick", 55, 1, CURDATE(), CURDATE(), 1);
-INSERT INTO Users (name, age, admin, createdAt, updatedAt, HouseholdId) VALUES ("Morty", 13, 0, CURDATE(), CURDATE(), 1);
-INSERT INTO Users (name, age, admin, createdAt, updatedAt, HouseholdId) VALUES ("Summer", 18, 0, CURDATE(), CURDATE(), 2);
-INSERT INTO Users (name, age, admin, createdAt, updatedAt, HouseholdId) VALUES ("Beth", 35, 1, CURDATE(), CURDATE(), 1);
-INSERT INTO Users (name, age, admin, createdAt, updatedAt, HouseholdId) VALUES ("Jerry", 35, 1, CURDATE(), CURDATE(), 2);
+INSERT INTO Users (name, age, admin, createdAt, updatedAt, HouseholdId) 
+VALUES 
+("Rick", 55, 1, CURDATE(), CURDATE(), 1),
+("Morty", 13, 0, CURDATE(), CURDATE(), 1),
+("Summer", 18, 0, CURDATE(), CURDATE(), 2),
+("Beth", 35, 1, CURDATE(), CURDATE(), 1),
+("Jerry", 35, 1, CURDATE(), CURDATE(), 2);
 
-INSERT INTO Chores (name, value, completed, createdAt, updatedAt, UserId) VALUES ("Clean room", 5, 0, CURDATE(), CURDATE(), 1);
-INSERT INTO Chores (name, value, completed, createdAt, updatedAt, UserId) VALUES ("Wash car", 10, 0, CURDATE(), CURDATE(), 2);
-INSERT INTO Chores (name, value, completed, createdAt, updatedAt, UserId) VALUES ("Dishes", 3, 0, CURDATE(), CURDATE(), 3);
+INSERT INTO Chores (name, value, photo, completed, createdAt, updatedAt, UserId) 
+VALUES 
+("Vacuum The Floor", 2, "./images/vacuum.png", 0, CURDATE(), CURDATE(), 1),
+("Clean The Bathroom", 3, "./images/bathroom.png", 0, CURDATE(), CURDATE(), 1),
+("Do The Laundry", 4, "./images/laundry.png", 0, CURDATE(), CURDATE(), 1),
+("Do The Dishes", 2, "./images/dishes.png", 0, CURDATE(), CURDATE(), 1),
+("Take Out The Trash", 2, "./images/trash.png", 0, CURDATE(), CURDATE(), 1),
+("Mow The Lawn", 10, "./images/mowing.png", 0, CURDATE(), CURDATE(), 1),
+("Walk The Dog", 4, "./images/dog.png", 0, CURDATE(), CURDATE(), 1);
