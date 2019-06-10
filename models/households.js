fc1642720580241314c57f705e6fdb1cc24ee121
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-06-10 06:55:31 -0500

    html routes updated

--- models/households.js
+++ models/households.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: [1]
       }
-    },
+    }
     // email: {
     //   type: DataTypes.STRING,
     //   allowNull: false,
