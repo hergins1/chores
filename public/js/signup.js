4499dbfb3823090163fb276a799a25a1194c8593
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-06-11 08:22:10 -0500

    signup.js ajax updated/NOT FIXED

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
   $("#submitBttn").on("click", function() {
     event.preventDefault();
-    let name = $("#userName").val().trim();
-    let household = $("#houseName").val().trim();
-    let email = $("#email").val().trim();
-    let password = $("#password").val().trim();
 
-    console.log(name);
-    console.log(household);
-    console.log(email);
-    console.log(password);
-    $.ajax("/api/households/create", {
-      type: "POST"
-    }).then(
-      function(result) {
-        console.log(result);
-      }
-    );
+    var householdInfo = {
+      name: $("#userName")
+        .val()
+        .trim(),
+      household: $("#houseName")
+        .val()
+        .trim(),
+      email: $("#email")
+        .val()
+        .trim(),
+      password: $("#password")
+        .val()
+        .trim()
+    };
+
+    // console.log(name);
+    // console.log(household);
+    // console.log(email);
+    // console.log(password);
+    $.ajax({
+      type: "POST",
+      url: "/api/households/create",
+      data: householdInfo
+    }).then(function(result) {
+      console.log(result);
+    });
   });
 });
