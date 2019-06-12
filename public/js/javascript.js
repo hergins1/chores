e8f09724ef7c3a504049b6fc10d9b677cf413b03
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-06-11 20:08:43 -0500

    user login route working

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -6,7 +6,3 @@ $(document).on("click", "#task-card", function(event) {
 $(".modal-close").on("click", function() {
   $(".modal").removeClass("is-active");
 });
-<<<<<<< HEAD
-=======
-
->>>>>>> f7d91192d60105c1d3bbb30853f00128dddf79c1
