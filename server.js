b99d1201144fc3225c6ae7e6e9a195622df35a90
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-06-13 11:41:37 -0500

    bugs fixed

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ require("./routes/householdsApiRoutes")(app);
 require("./routes/usersApiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
